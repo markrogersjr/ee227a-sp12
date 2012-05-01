ee89a4cd568cba57d99fa1a22d5ec4bd39440f0b
Mark Rogers <markrogersjr@gmail.com>
2012-05-01 14:27:15 -0700

    fixed bug in quantile_ranking.m

--- code/quantile_ranking.m
+++ code/quantile_ranking.m
@@ -1,18 +1,20 @@
 function [X Y S] = quantile_ranking(X,Y,S)
 
+n = size(X,1);
+p = size(X,2);
 L = (X-S) / 2;
 H = (X+S) / 2;
 LH = [L;H];
-Irandom = randperm(numel(Y));
+Irandom = randperm(2*n);
 [R Iinverse] = sort(Irandom);
 LH = LH(Irandom,:);
-for i = 1:size(LH,2)
+for i = 1:p
 	[vals ranks] = sort(LH(:,i));
 	LH(:,i) = norminv(ranks / (numel(ranks)+1),0,1);
 end
 LH = LH(Iinverse,:);
-L = LH([1:(size(LH,1)/2)],:);
-H = LH([1:(size(LH,1)/2)]+size(LH,1)/2,:);
+L = LH([1:n],:);
+H = LH([1:n]+n,:);
 X = (L+H) / 2;
 S = abs(L-H) / 2;
 
