bc8c96d92d30cab56b857af62779afe540ba2f63
james <james@james-laptop.(none)>
2012-05-10 05:45:22 -0700

    changed code for making latex table

--- report/create_table.py
+++ report/create_table.py
@@ -29,14 +29,15 @@ if __name__ == "__main__":
 
     ### print non-zero features in nice table
     ## beginning parts of table
-    table = r"""\begin{center}\n\begin{tabular}{ | l | l |}\n\hline"""
+    table = r"""\begin{center}""" + "\n" + r"""\begin{tabular}{ | l | l |}""" + "\n" + r"""\hline""" + "\n"
+    table = table + r"""\textbf{feature name} & \textbf{beta} \\""" + "\n"
     for j in zip(feature_names,weight):
         if j[1] > 0.0:
-            table = table + r"&" + j[0] + r" & " + repr(j[1]) + r"\\" + r"\n"
-    table = table +  r"""\hline\n\end{tabular}\n\end{center}"""
+            table = table + j[0] + r" & " + ("%.2f" % j[1]) + r"\\" + "\n"
+    table = table +  r"""\hline""" + "\n" + """\end{tabular}""" + "\n" + """\end{center}"""
     print table
+    table = table.replace("_"," ")
     output_file = sys.argv[1].split('/').pop().split('.')[0] + ".tex"
     g = open(output_file,'w')
-    table = table.split('\n')
-    g.writelines(table)
+    g.write(table)
     g.close()
